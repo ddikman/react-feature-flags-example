425f8425ac2aac16a0fde864cb8db0d2e1dab332
David Dikman <ddikman@gmail.com>
2023-08-05 10:01:31 +0900

    Unleash working

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,28 +1,45 @@
+'use client'
+
 import './globals.css'
 import { Inter } from 'next/font/google'
 
+import { FlagProvider } from '@unleash/proxy-client-react';
+
+const config = {
+  url: 'http://localhost:4242/api/frontend',
+  clientKey: '*:development.32333e8db319d00cfb3a8e766457a11af4a0d4249145a9291e852657', // update this with an API key you have created
+  refreshInterval: 15,
+  appName: 'default'
+
+};
+
 const inter = Inter({ subsets: ['latin'] })
 
-export const metadata = {
-  title: 'React Feature Flag Example',
-  description: 'Example to showcase feature flag usage in React',
-}
+// export const metadata = {
+//   title: 'React Feature Flag Example',
+//   description: 'Example to showcase feature flag usage in React',
+// }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <main>
-          {children}
+        {
+          // Added this FlagProvider
+        }
+        <FlagProvider config={config}>
+          <main>
+            {children}
 
-          <footer>
-            <hr className='my-4' />
-            <div className='text-sm flex justify-between'>
-              <span>Made by <a target="_blank" href="https://greycastle.se">David</a></span>
-              <a href="https://github.com/ddikman/react-feature-flags-example" target='_blank'>Fork this (GitHub)</a>
-            </div>
-          </footer>
-        </main>
+            <footer>
+              <hr className='my-4' />
+              <div className='text-sm flex justify-between'>
+                <span>Made by <a target="_blank" href="https://greycastle.se">David</a></span>
+                <a href="https://github.com/ddikman/react-feature-flags-example" target='_blank'>Fork this (GitHub)</a>
+              </div>
+            </footer>
+          </main>
+        </FlagProvider>
       </body>
     </html>
   )
--- src/app/profile-card.js
+++ src/app/profile-card.js
@@ -2,9 +2,13 @@ import Card from '@/app/card';
 import ProfileLink from '@/app/profile-link';
 import getFeatureFlagValue from '@/app/feature-flags/get-feature-flag-value';
 
+import { useFlag } from '@unleash/proxy-client-react';
+
+
 export default function ProfileCard({ user }) {
 
-  const featureEnabled = getFeatureFlagValue('profile-link');
+  // const featureEnabled = getFeatureFlagValue('profile-link');
+  const featureEnabled = useFlag('profile-link');
   const username = featureEnabled
     ? <ProfileLink username={user.username} label={user.handle} />
     : <span>{user.handle}</span>;
