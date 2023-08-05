2ee8c8ff447074ad8f6886b596b278824cc05aa9
David Dikman <ddikman@gmail.com>
2023-08-05 10:07:21 +0900

    Using client component and removing head tags

---
+++ src/app/footer.js
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return <footer>
+    <hr className='my-4' />
+    <div className='text-sm flex justify-between'>
+      <span>Made by <a target="_blank" href="https://greycastle.se">David</a></span>
+      <a href="https://github.com/ddikman/react-feature-flags-example" target='_blank'>Fork this (GitHub)</a>
+    </div>
+  </footer>;
+}
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,12 +1,10 @@
+'use client'
+
 import './globals.css'
 import { Inter } from 'next/font/google'
+import Footer from '@/app/footer'
 
 const inter = Inter({ subsets: ['latin'] })
-
-export const metadata = {
-  title: 'React Feature Flag Example',
-  description: 'Example to showcase feature flag usage in React',
-}
 
 export default function RootLayout({ children }) {
   return (
@@ -14,14 +12,7 @@ export default function RootLayout({ children }) {
       <body className={inter.className}>
         <main>
           {children}
-
-          <footer>
-            <hr className='my-4' />
-            <div className='text-sm flex justify-between'>
-              <span>Made by <a target="_blank" href="https://greycastle.se">David</a></span>
-              <a href="https://github.com/ddikman/react-feature-flags-example" target='_blank'>Fork this (GitHub)</a>
-            </div>
-          </footer>
+          <Footer />
         </main>
       </body>
     </html>
